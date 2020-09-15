5123a49594ebbdd5a15db88bcf18459162158563
Murunga Kibaara <murungakibaara@outlook.com>
2020-09-15 21:10:34 +0300

    [Remove Instance from gitignore file]

---
+++ instance/config.py
@@ -0,0 +1,23 @@
+'''App Configurations'''
+import os
+
+class Config(object):
+    """Main Configurations"""
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'xac,oj\xd4$\xee\x13\xdezZI\xf2\x9b\xd6`\x1e_"\xb1\x1ey\xc1I'
+
+class DevelopmentConfig(Config):
+    """Development Configurations"""
+    DEBUG = True
+    DATABASE_NAME = "mpesa"
+    DATABASE_URL = os.getenv("DATABASE")
+
+class ProductionConfig(Config):
+    """Production Configurations"""
+    DEBUG = False
+
+APP_CONFIG = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+}
