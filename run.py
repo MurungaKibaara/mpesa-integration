ceac0fe6ec34987225ad02403a4941c56b6f2a9f
Murunga Kibaara <murungakibaara@outlook.com>
2020-09-16 20:09:08 +0300

    [Add gunicorn logger]

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 
 '''Application Entry Point'''
 import os
+import logging
 from app import create_app
 
 APP = create_app(os.getenv("FLASK_ENV"))
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    APP.logger.handlers = gunicorn_logger.handlers
+    APP.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
     APP.run()
